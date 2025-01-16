8d8756e3bb1164bce4236e157b14a5519bc0b525
tosiab <mtabednarz@gmail.com>
2025-01-15 21:43:08 -0500

    hshshssd

--- SGRandomWalk.java
+++ SGRandomWalk.java
@@ -6,19 +6,29 @@ public class SGRandomWalk {
         field.setColor(Color.BLUE);
         field.draw();
         field.fill();
-        Ellipse circle = new Ellipse(75,75,100,100);
+        Ellipse circle = new Ellipse(100,100,100,100);
         circle.setColor(Color.PINK);
         circle.draw();
-        circle.fill();
         Ellipse dot = new Ellipse(125,125,10,10);
         dot.setColor(Color.MAGENTA);
         dot.draw();
         dot.fill();
         while(escaped==false){
+            try { Thread.sleep(100); }
+            catch(Exception ex) {};
             int randx = (int)(Math.random()*21)-10;
             int randy = (int)(Math.random()*21)-10;
             tries += 1;
             dot.translate(randx,randy);
+            int x = dot.getX()-100;
+            int y = dot.getY()-100;
+            int xx = x*x;
+            int yy = y*y;
+            int d = (int) Math.sqrt(xx+yy);
+            if(d>100){
+                escaped = true;
+                System.out.println("It took "+ tries+ " tries to escape.");
+            }
         }
 
     }
